f2e814970d150320e7135af27e615a417b7259eb
zjgarvey <zjgarvey@gmail.com>
2024-04-15 10:53:06 -0500

    Adds static option for opt-125m-awq model

--- e2eshark/onnx/models/opt-125m-awq/model.py
+++ e2eshark/onnx/models/opt-125m-awq/model.py
@@ -1,6 +1,7 @@
-import numpy, torch, sys
+import numpy, torch, sys, os
 import onnxruntime
 import onnx
+from onnxruntime.tools.onnx_model_utils import make_dim_param_fixed, fix_output_shapes
 
 # import from e2eshark/tools to allow running in current dir, for run through
 # run.pl, commutils is symbolically linked to allow any rundir to work
@@ -10,22 +11,69 @@ from commonutils import E2ESHARK_CHECK_DEF
 # Create an instance of it for this test
 E2ESHARK_CHECK = dict(E2ESHARK_CHECK_DEF)
 
-
 # The generated or checked in onnx file must always be called model.onnx
 # the tools/stubs/onnxmodel.py is appended to model.py
 # to form runmodel.py in the rundirectory which is then taken
 # through flow
 
-# set some variable parameters (dynamic dims)
-batch_size = 3 
-sequence_length = 7 
-past_sequence_length = 0 # setting this parameter to anything other than 0 results in an error
+# If you want to run the test with a static model.onnx, set run_as_static = True
+run_as_static = False
+dim_params = (
+    "batch_size",
+    "sequence_length",
+    "past_sequence_length",
+    "past_sequence_length + 1",
+)
+dim_values = (12, 7, 0, 1)
+pv_zip = zip(dim_params, dim_values)
+pv = dict(pv_zip)
+
+model = onnx.load("model.onnx")
+isDynamic = bool(model.graph.input[0].type.tensor_type.shape.dim[0].dim_param)
+
+if run_as_static:
+    if isDynamic:
+        print("Saving dynamic model...")
+        onnx.save(model, "../../../../onnx/models/opt-125m-awq/dynamic_model.onnx")
+        print("\t dynamic model saved as dynamic_model.onnx")
+
+        print("Setting model dim_params:")
+        for p, v in zip(dim_params, dim_values):
+            make_dim_param_fixed(model.graph, p, v)
+            print("\t", p, " = ", v)
+
+        fix_output_shapes(model)
+        print("Overwriting file contents of model.onnx...")
+        onnx.save(model, "model.onnx")
+        print("\t model.onnx is now a static model.")
+elif not isDynamic:
+    print(
+        "model.onnx is currently static.\nAttempting to retrieve dynamic model from dynamic_model.onnx..."
+    )
+    try:
+        model = onnx.load("../../../../onnx/models/opt-125m-awq/dynamic_model.onnx")
+        print("\t dynamic model found! \nSaving dynamic model as model.onnx...")
+        onnx.save(model, "model.onnx")
+        print("\t dynamic model saved as model.onnx.")
+        os.remove("../../../../onnx/models/opt-125m-awq/dynamic_model.onnx")
+    except OSError as e:
+        print(e, "\nNo dynamic_model found. Running static model instead.")
 
 # get some model inputs
-model_inputs = [numpy.random.rand(batch_size, sequence_length).astype(numpy.int64)] # input_ids
-model_inputs.append(numpy.random.rand(batch_size, past_sequence_length + 1).astype(numpy.int64)) # attention_mask
-for i in range(2*12):
-    model_inputs.append(numpy.random.rand(batch_size, 12, past_sequence_length, 64).astype(numpy.float32)) # 12 key/value pairs
+model_inputs = [
+    numpy.random.rand(pv["batch_size"], pv["sequence_length"]).astype(numpy.int64)
+]  # input_ids
+model_inputs.append(
+    numpy.random.rand(pv["batch_size"], pv["past_sequence_length + 1"]).astype(
+        numpy.int64
+    )
+)  # attention_mask
+for i in range(2 * 12):
+    model_inputs.append(
+        numpy.random.rand(pv["batch_size"], 12, pv["past_sequence_length"], 64).astype(
+            numpy.float32
+        )
+    )  # 12 key/value pairs
 
 # start a session
 session = onnxruntime.InferenceSession("model.onnx", None)
@@ -46,4 +94,3 @@ E2ESHARK_CHECK["output"] = [torch.from_numpy(arr) for arr in model_outputs]
 
 print("Input:", E2ESHARK_CHECK["input"])
 print("Output:", E2ESHARK_CHECK["output"])
-
